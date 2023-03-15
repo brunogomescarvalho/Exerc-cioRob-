85cfd953b1e519c1d666d815a5de7d925d7930f3
brunogomescarvalho <7bruno8@gmail.com>
2023-03-15 03:33:59 -0300

    incluido tamanho e validacoes limite do grid

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿namespace RoboTupiniquim
+﻿using System.Linq;
+namespace RoboTupiniquim
 {
     public class Program
     {
@@ -15,39 +16,66 @@
             char roboDaVez = '1';
 
             Console.Clear();
-            System.Console.WriteLine("--- Robô InterEspacial ---\n");
+            Console.WriteLine("--- Robô InterEspacial ---\n");
 
-            System.Console.Write("Informe em número inteiro a posição inicial X do robô 1: ");
-            robo1X = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Informe a medida do plano cartesiano separado por virgula: ex (5,5)");
+            string plano = Console.ReadLine()!;
 
-            System.Console.Write("Informe em número inteiro a posição inicial Y do robô 1: ");
-            robo1Y = int.Parse(Console.ReadLine()!);
+            var grid = plano.Split(",");
+            var gridX = int.Parse(grid[0]);
+            var gridY = int.Parse(grid[1]);
+            bool coordenadasValidas = true;
 
-            System.Console.Write("Informe a direção cardial do robô 1: [ N , S , L , O  ]: ");
-            robo1Direcao = Convert.ToChar(Console.ReadLine()!.ToUpper());
+            do
+            {
+                Console.Write("Informe em número inteiro a posição inicial X do robô 1: ");
+                robo1X = int.Parse(Console.ReadLine()!);
+
+                Console.Write("Informe em número inteiro a posição inicial Y do robô 1: ");
+                robo1Y = int.Parse(Console.ReadLine()!);
+
+                Console.Write("Informe a direção cardial do robô 1: [ N , S , L , O  ]: ");
+                robo1Direcao = Convert.ToChar(Console.ReadLine()!.ToUpper());
+
+                Console.Clear();
+                Console.Write("Informe em número inteiro a posição inicial X do robô 2: ");
+                robo2X = int.Parse(Console.ReadLine()!);
+
+                Console.Write("Informe em número inteiro a posição inicial Y do robô 2: ");
+                robo2Y = int.Parse(Console.ReadLine()!);
+
+                System.Console.Write("Informe a direção cardial do robô 2: [ N , S , L , O  ]: ");
+                robo2Direcao = Convert.ToChar(Console.ReadLine()!.ToUpper());
+
+                coordenadasValidas = VerificaCoordenadas(gridX, gridY, robo1X, robo1Y, robo2X, robo2Y);
+                if (!coordenadasValidas)
+                {
+                    Console.WriteLine($"Coordenadas fora do plano cartesiano do tamanho {plano}\n Tecle para continuar");
+                    Console.ReadKey();
+                    continue;
+                }
+
+            } while (!coordenadasValidas);
+
 
             Console.Clear();
-            System.Console.Write("Informe em número inteiro a posição inicial X do robô 2: ");
-            robo2X = int.Parse(Console.ReadLine()!);
-
-            System.Console.Write("Informe em número inteiro a posição inicial Y do robô 2: ");
-            robo2Y = int.Parse(Console.ReadLine()!);
-
-            System.Console.Write("Informe a direção cardial do robô 2: [ N , S , L , O  ]: ");
-            robo2Direcao = Convert.ToChar(Console.ReadLine()!.ToUpper());
-
-            Console.Clear();
-            System.Console.WriteLine("Para movimentar o robô digite uma ou mais coordenadas. Ex: MMMEEDD\n" +
+            Console.WriteLine("Para movimentar o robô digite uma ou mais coordenadas. Ex: MMMEEDD\n" +
                    "Para avançar tecle M, para direita tecle D, para esquerda tecle E.\nTecle para iniciar.");
             Console.ReadKey();
 
-
             while (continuar)
             {
+                int _robo1X = 0;
+                int _robo1Y = 0;
+                int _robo2X = 0;
+                int _robo2Y = 0;
+
                 roboDaVez = roboUm ? '1' : '2';
                 Console.Clear();
+
                 InformaCoordenadas(robo1X, robo1Y, robo1Direcao, robo2X, robo2Y, robo2Direcao, roboDaVez);
-                System.Console.Write("Informe as coordenadas:\n => ");
+                Console.Write("Informe as coordenadas:\n => ");
+
                 string coordenadas = Console.ReadLine()!;
 
                 foreach (var instrucao in coordenadas.ToUpper())
@@ -65,11 +93,14 @@
                                 break;
 
                             case 'M':
-                                (robo1X, robo1Y) = Mover(robo1Direcao, robo1Y, robo1X);
+                                (_robo1X, _robo1Y) = Mover(robo1Direcao, robo1Y, robo1X);
+
+                                robo1X = (_robo1X <= gridX && _robo1X >= 0) ? _robo1X : robo1X;
+                                robo1Y = (_robo1Y <= gridY && _robo1Y >= 0) ? _robo1Y : robo1Y;
                                 break;
 
                             default:
-                                System.Console.WriteLine("Opção inválida");
+                                Console.WriteLine("Opção inválida");
                                 Console.ReadKey();
                                 break;
                         }
@@ -87,35 +118,54 @@
                                 break;
 
                             case 'M':
-                                (robo2X, robo2Y) = Mover(robo2Direcao, robo2Y, robo2X);
+                                (_robo2X, _robo2Y) = Mover(robo2Direcao, robo2Y, robo2X);
+
+                                robo2X = (_robo2X <= gridX && _robo2X >= 0) ? _robo2X : robo2X;
+                                robo2Y = (_robo2Y <= gridY && _robo2Y >= 0) ? _robo2Y : robo2Y;
                                 break;
 
                             default:
-                                System.Console.WriteLine("Opção inválida");
+                                Console.WriteLine("Opção inválida");
                                 Console.ReadKey();
                                 break;
                         }
                     }
                 }
+
+                if ((_robo2X > gridX || _robo2Y > gridY) || (_robo2X < 0 || _robo2Y < 0) || (_robo1X > gridX || _robo1Y > gridY) || (_robo1Y < 0 || _robo1X < 0))
+                {
+                    Console.WriteLine($"Coordenadas no limite do plano cartesiano do tamanho {plano}\nTecle para continuar!");
+                    Console.ReadKey();
+                    roboUm = !roboUm;
+                    continue;
+                }
+
                 InformaCoordenadasAposMover(robo1X, robo1Y, robo1Direcao, robo2X, robo2Y, robo2Direcao, roboDaVez);
                 roboUm = !roboUm;
-
             }
         }
 
-        
 
-        private static void InformaCoordenadas(int robo1X, int robo1Y, char robo1Direcao, int robo2X, int robo2Y, char robo2Direcao, char roboDaVez)
+
+        static bool VerificaCoordenadas(int gridX, int gridY, int robo1X, int robo1Y, int robo2X, int robo2Y)
+        {
+            return (robo1Y > gridY || robo2Y > gridY ? false : robo1X > gridX || robo2X > gridX ? false : robo1X < 0 || robo2X < 0 ? false : robo1Y < 0 || robo2Y < 0 ? false : true);
+        }
+
+
+        static void InformaCoordenadas(int robo1X, int robo1Y, char robo1Direcao, int robo2X, int robo2Y, char robo2Direcao, char roboDaVez)
         {
             System.Console.WriteLine($"Você está controlando o robô [{roboDaVez}]\nSua atual posição é {(roboDaVez == '1' ? robo1X : robo2X, roboDaVez == '1' ? robo1Y : robo2Y, roboDaVez == '1' ? robo1Direcao : robo2Direcao)}");
         }
 
-        private static void InformaCoordenadasAposMover(int robo1X, int robo1Y, char robo1Direcao, int robo2X, int robo2Y, char robo2Direcao, char roboDaVez)
+
+        static void InformaCoordenadasAposMover(int robo1X, int robo1Y, char robo1Direcao, int robo2X, int robo2Y, char robo2Direcao, char roboDaVez)
         {
             System.Console.WriteLine($"O robô [{roboDaVez}] foi para posição {(roboDaVez == '1' ? robo1X : robo2X, roboDaVez == '1' ? robo1Y : robo2Y, roboDaVez == '1' ? robo1Direcao : robo2Direcao)}");
             System.Console.Write("\nTecle para continuar");
             Console.ReadKey();
         }
+
 
         static char Direita(char direcao)
         {
@@ -138,6 +188,7 @@
             return direcao;
         }
 
+
         static char Esquerda(char direcao)
         {
             if (direcao == 'N')
@@ -158,6 +209,7 @@
             }
             return direcao;
         }
+
 
         static (int, int) Mover(char direcao, int Y, int X)
         {
